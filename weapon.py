8b2071e2a75a14671f753410a49071e57c2a8b1b
Haitian Chen <me@icefires.cc>
2021-09-14 17:42:41 -0700

    Fixed ammo type

--- weapon.py
+++ weapon.py
@@ -136,7 +136,7 @@ def fetch_ammo_list(ammo_list):
             shot_type = [i.strip().lower().replace(" ", "_") for i in contents[2].text.split(",")]
             ammo_type = contents[0].text.lower().replace(" ", "_")
             ammo_entry.append({
-                "ammo_type": ammo_type if ammo_type != "amor_ammo" else "ammor_ammo",
+                "ammo_type": ammo_type if ammo_type != "amor_ammo" else "armor_ammo",
                 "capacity": int(contents[1].text),
                 "shot_type": shot_type if shot_type[0] != "" else []
             })
